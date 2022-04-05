81211443478e5d4db4a759c8f4e749b767732c4d
crazy4devops <crazy4devops@gmail.com>
2022-04-05 12:59:07 +0530

    added code

--- providers.tf
+++ providers.tf
@@ -12,14 +12,3 @@ terraform {
   }
 }
 
-
-docker network create \
-  --driver=bridge \
-  --subnet=182.18.0.1/24 \
-  --gateway=182.18.0.1 \
-  wp-mysql-network
-
-  Name: wp-mysql-network
-Driver: bridge
-subnet: 182.18.0.1/24
-Gateway: 182.18.0.1
