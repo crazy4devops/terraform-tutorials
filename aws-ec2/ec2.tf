3c202092ec80c1884e006319f5707a07be79b573
crazy4devops <crazy4devops@gmail.com>
2022-04-13 13:25:12 +0530

    updated the code

--- aws-ec2/ec2.tf
+++ aws-ec2/ec2.tf
@@ -1,14 +1,22 @@
 resource "aws_instance" "dev-ec2" {
   ami           = data.aws_ami.amzlinux.id
-  instance_type = var.instance_type
+  #instance_type = var.instance_type
+  instance_type = var.instance_type_list[0]
+  #instance_type = var.instance_type_map["prod"]
   user_data     = file("${path.module}/install-web.sh")
   key_name      = var.instance_keypair
+  count = 3
   vpc_security_group_ids = [
     aws_security_group.ssh_sg.id,
     aws_security_group.web_sg.id
   ]
   tags = {
-    Name = "DemoWebApp"
+    Name = "DemoWebApp-${count.index}"
   }
 
 }
+#aws_instance.dev-ec2.0,1,2
+#count = 5
+#0,1,2,3,4
+#count = 2
+#0,1
--- aws-ec2/variables.tf
+++ aws-ec2/variables.tf
@@ -14,4 +14,23 @@ variable "instance_keypair" {
   default = "terraform-key"
 
 }
+variable "instance_type_list" {
+  description = "instnace type is list"
+  type = list(string)
+  default = ["t3.micro","t3.small", "t3.large"]
+}
+# var.instance_type_list[2]
+
+variable "instance_type_map" {
+  description = "instance type is map"
+  type = map(string)
+  default = {
+    "dev" = "t3.micro"
+    "qa" = "t3.small"
+    "prod" = "t3.large"
+  }
+  
+}
+#var.instnace_type_map["prod"]
+
 
