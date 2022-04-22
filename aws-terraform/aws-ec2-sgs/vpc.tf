e507572b287a0d9496df720cda1c5a19ead8e2e6
Bitislit <tejasbitla@gmail.com>
2022-04-22 15:24:45 +0530

    added null resources

--- aws-terraform/aws-ec2-sgs/vpc.tf
+++ aws-terraform/aws-ec2-sgs/vpc.tf
@@ -8,10 +8,10 @@ module "vpc" {
   private_subnets = var.vpc_private_subnets
   public_subnets  = var.vpc_public_subnets
 
-  create_database_subnet_group       = var.vpc_create_database_subnet_group
-  create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
-  database_subnets                   = var.vpc_database_subnets
-  create_database_nat_gateway_route  = true
+  # create_database_subnet_group       = var.vpc_create_database_subnet_group
+  # create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
+  # database_subnets                   = var.vpc_database_subnets
+  # create_database_nat_gateway_route  = true
 
   enable_nat_gateway = var.vpc_enable_nat_gateway
   single_nat_gateway = var.vpc_single_nat_gateway
@@ -28,7 +28,7 @@ module "vpc" {
   private_subnet_tags = {
     Name = "Private Subnets"
   }
-  database_subnet_tags = {
-    Name = "database Subnets"
-  }
+  # database_subnet_tags = {
+  #   Name = "database Subnets"
+  # }
 }
