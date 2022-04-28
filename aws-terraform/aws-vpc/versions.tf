5d94f93dbae067bdc70e208cf923e390379a9f3d
tejasbitla <tejasbitla@gmail.com>
2022-04-28 07:41:02 +0100

    added backend state files

--- aws-terraform/aws-vpc/versions.tf
+++ aws-terraform/aws-vpc/versions.tf
@@ -1,10 +1,20 @@
 terraform {
-  required_version = "~> 0.14"
+  required_version = "~> 1.1.7"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.8.0"
     }
+  }
+
+  backend "s3" {
+    bucket = "terraform-bkt123"
+    key    = "dev/proj1/terraform.tfstate"
+    region = "us-east-1"
+
+    # Enable during Step-09     
+    # For State Locking
+    dynamodb_table = "dev-proj1-vpc"    
   }
 }
 
--- aws-terraform/aws-vpc/vpc.tf
+++ aws-terraform/aws-vpc/vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "../modules/aws-vpc-old"
+  source = "terraform-aws-modules/vpc/aws"
 
   name = var.vpc_name
   cidr = var.vpc_cidr_block
@@ -8,10 +8,10 @@ module "vpc" {
   private_subnets = var.vpc_private_subnets
   public_subnets  = var.vpc_public_subnets
 
-  create_database_subnet_group       = var.vpc_create_database_subnet_group
-  create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
-  database_subnets                   = var.vpc_database_subnets
-  create_database_nat_gateway_route  = true
+  # create_database_subnet_group       = var.vpc_create_database_subnet_group
+  # create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
+  # database_subnets                   = var.vpc_database_subnets
+  # create_database_nat_gateway_route  = true
 
   enable_nat_gateway = var.vpc_enable_nat_gateway
   single_nat_gateway = var.vpc_single_nat_gateway
